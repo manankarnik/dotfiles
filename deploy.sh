23f5064836f472422d9ecbfa081be9f29c790f4d
Manan Karnik <manankarnik@gmail.com>
2025-05-16 17:55:51 +0530

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+DOTFILES_DIR="$HOME/dotfiles"
+CONFIG_DIR="$HOME/.config"
+
+home_link() {
+    local files=("$@")
+    for file in "${files[@]}"; do
+        ln -svf "$DOTFILES_DIR/$file" "$HOME/$file"
+    done
+}
+
+config_link() {
+    local files=("$@")
+    mkdir -p "$HOME/.config"
+    for file in "${files[@]}"; do
+        ln -svf "$DOTFILES_DIR/$file" "$CONFIG_DIR/$file"
+    done
+}
+
+home_link ".xinitrc"
+config_link "bspwm"
