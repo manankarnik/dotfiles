356f41b3747934404af7c37c3bcf9977c63fbf60
Manan Karnik <manankarnik@gmail.com>
2025-05-16 21:33:27 +0530

    Add polybar

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,22 @@ CONFIG_DIR="$HOME/.config"
 home_link() {
     local files=("$@")
     for file in "${files[@]}"; do
-        ln -svf "$DOTFILES_DIR/$file" "$HOME/$file"
+        if [ -e "$HOME/$file" ]; then
+            mv "$HOME/$file" "$HOME/.cache/$file.old"
+        fi
+        ln -svi "$DOTFILES_DIR/$file" "$HOME/$file"
     done
 }
 
 config_link() {
     local files=("$@")
-    mkdir -p "$HOME/.config"
     for file in "${files[@]}"; do
-        ln -svf "$DOTFILES_DIR/$file" "$CONFIG_DIR/$file"
+        if [ -e "$CONFIG_DIR/$file" ]; then
+            mv "$CONFIG_DIR/$file" "$HOME/.cache/$file.old"
+        fi
+        ln -svi "$DOTFILES_DIR/$file" "$CONFIG_DIR/$file"
     done
 }
 
-home_link ".xinitrc"
-config_link "bspwm" "sxhkd" "kitty" "rofi"
+home_link .xinitrc
+config_link bspwm sxhkd kitty rofi polybar
